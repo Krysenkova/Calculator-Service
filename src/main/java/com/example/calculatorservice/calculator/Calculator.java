2cb2bab0f5d48d745caec710158fb6ef77c604c2
Ekaterina <e.krysenkova@avm.de>
2022-02-05 21:10:38 +0100

    add small integration test

--- src/main/java/com/example/calculatorservice/calculator/Calculator.java
+++ src/main/java/com/example/calculatorservice/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package com.example.calculatorservice.calculator;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -7,15 +8,18 @@ import java.math.RoundingMode;
 public class Calculator {
 
     private final double MWST = 0.19;
+
+    @Positive(message = "The price should be a positive value and bigger than null")
     private final Double price;
 
-    public Calculator(@Positive Double price) {
+    public Calculator(Double price) {
         this.price = price;
     }
 
     public Double calculateMwSt() {
         return price * MWST;
     }
+
     public Double calculatePrice() {
         Double priceWithMwSt = price + calculateMwSt();
         BigDecimal bd = new BigDecimal(priceWithMwSt).setScale(2,RoundingMode.UP);
